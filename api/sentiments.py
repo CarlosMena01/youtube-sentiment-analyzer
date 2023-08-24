1eaa2c0c518da58eee1159fb9ef5ec541bab4146
CarlosMena01 <cmena@unal.edu.co>
2023-08-24 16:40:02 -0500

    [ADD] Sentiment score function

--- api/sentiments.py
+++ api/sentiments.py
@@ -1,6 +1,11 @@
 from googleapiclient.discovery import build
+
 import pandas as pd
+
 from decouple import config
+
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from scipy.special import softmax
 
 api_key = config("API_KEY")
 youtube = build("youtube", "v3", developerKey=api_key)
@@ -32,3 +37,21 @@ def get_youtube_video_comments(video_id:str):
 
     df = pd.DataFrame(comments)
     return df
+
+MODEL = f"cardiffnlp/twitter-roberta-base-sentiment"
+tokenizer = AutoTokenizer.from_pretrained(MODEL)
+model = AutoModelForSequenceClassification.from_pretrained(MODEL)
+
+def polarity_scores_roberta(example):
+    encoded_text = tokenizer(example, return_tensors='pt')
+    output = model(**encoded_text)
+    scores = output[0][0].detach().numpy()
+    scores = softmax(scores)
+    scores_dict = {
+        'roberta_neg' : scores[0],
+        'roberta_neu' : scores[1],
+        'roberta_pos' : scores[2]
+    }
+    return scores_dict
+
+print(polarity_scores_roberta("I am so happyyyy!!"))
