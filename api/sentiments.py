70540d00fd59851639396bce1799d6da7f76d401
CarlosMena01 <cmena@unal.edu.co>
2023-08-24 16:41:49 -0500

    [DOC] Comment the code

--- api/sentiments.py
+++ api/sentiments.py
@@ -1,29 +1,33 @@
+# Import necessary libraries
 from googleapiclient.discovery import build
-
 import pandas as pd
-
 from decouple import config
-
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from scipy.special import softmax
 
+# Get API key from environment variables
 api_key = config("API_KEY")
+
+# Create a YouTube API client
 youtube = build("youtube", "v3", developerKey=api_key)
 
-
-def get_youtube_video_comments(video_id:str):
-    """This function use an video Id from YouTube and return a pandas df with the comments information
+# Function to retrieve comments from a YouTube video
+def get_youtube_video_comments(video_id: str):
+    """
+    This function takes a YouTube video ID and returns comments information in a pandas DataFrame.
 
     Args:
-        video_id (str): Id of the YouTube video
+        video_id (str): ID of the YouTube video.
 
     Returns:
-        pandas.df: [ID][Author][Comment text][Date][Like counter]
+        pandas.DataFrame: Contains columns - 'Comment ID', 'Author', 'Comment Text', 'Date', 'Like Count'.
     """
+    # Retrieve comments for the given video
     response = youtube.commentThreads().list(part="snippet,replies", maxResults=100, videoId=video_id).execute()
 
     comments = []
 
+    # Extract relevant comment information from the response
     for item in response["items"]:
         comment_info = {
             "Comment ID": item["id"],
@@ -35,22 +39,40 @@ def get_youtube_video_comments(video_id:str):
 
         comments.append(comment_info)
 
+    # Create a pandas DataFrame from the comment information
     df = pd.DataFrame(comments)
     return df
 
-MODEL = f"cardiffnlp/twitter-roberta-base-sentiment"
+# Sentiment analysis model setup
+MODEL = "cardiffnlp/twitter-roberta-base-sentiment"
 tokenizer = AutoTokenizer.from_pretrained(MODEL)
 model = AutoModelForSequenceClassification.from_pretrained(MODEL)
 
 def polarity_scores_roberta(example):
+    """
+    This function calculates sentiment scores using the Roberta model.
+
+    Args:
+        example (str): Text input for sentiment analysis.
+
+    Returns:
+        dict: Sentiment scores for negative, neutral, and positive sentiments.
+    """
+    # Encode the input text using the tokenizer
     encoded_text = tokenizer(example, return_tensors='pt')
+
+    # Pass the encoded text through the model
     output = model(**encoded_text)
+
+    # Extract and normalize sentiment scores
     scores = output[0][0].detach().numpy()
     scores = softmax(scores)
+    
+    # Create a dictionary with sentiment scores
     scores_dict = {
-        'roberta_neg' : scores[0],
-        'roberta_neu' : scores[1],
-        'roberta_pos' : scores[2]
+        'roberta_neg': scores[0],
+        'roberta_neu': scores[1],
+        'roberta_pos': scores[2]
     }
     return scores_dict
 
